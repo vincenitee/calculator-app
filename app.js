5a84ebe2254baa740bc107626626dfabb64f12ad
Vincent <vincentbolinget@gmail.com>
2025-06-11 23:10:05 +0800

    Add removeLastElement function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let firstOperand = "";
 let secondOperand = "";
-let result = 0;
+let result = null;
 let operator = null;
 
 const selectById = (id) => document.getElementById(id);
@@ -39,9 +39,13 @@ function appendNumber(number) {
     if (!operator) {
         firstOperand = firstOperand === 0 ? number : firstOperand + number;
         displayValue = firstOperand;
+
+        console.log(`First Operand: ${firstOperand}`);
     } else {
         secondOperand = secondOperand === 0 ? number : secondOperand + number;
         displayValue = secondOperand;
+
+        console.log(`Second Operand: ${secondOperand}`);
     }
 
     updateDisplay(displayValue);
@@ -75,17 +79,34 @@ function updateDisplay(value) {
     display.value = value;
 }
 
+function removeLastElement(value){
+    return value.slice(0, -1);
+}
+
 digitButtons.forEach((button) => {
     button.addEventListener('click', () => { appendNumber(button.textContent) })
 });
 
 operatorButtons.forEach((button) => {
-    button.addEventListener('click', () => { operator = button.textContent })
+    button.addEventListener('click', () => { 
+        if(firstOperand && secondOperand){
+            result = operate(operator, firstOperand, secondOperand);
+
+            console.log(result);
+            firstOperand = roundResult(result);
+            secondOperand = "";
+            updateDisplay(firstOperand);
+        }
+
+        operator = button.textContent;
+        console.log(`<operator> ${operator}`);
+    })
 });
 
 equalButton.addEventListener('click', () => {
-    let result = operate(operator, firstOperand, secondOperand);
-    updateDisplay(result);
+    if (firstOperand === '' || secondOperand === '') return;
+    result = operate(operator, firstOperand, secondOperand);
+    updateDisplay(roundResult(result));
 });
 
 clearButton.addEventListener('click', () => {
@@ -95,10 +116,10 @@ clearButton.addEventListener('click', () => {
 
 deleteButton.addEventListener('click', () => {
     if (!operator) {
-        firstOperand = firstOperand.slice(0, -1);
+        firstOperand = removeLastElement(firstOperand);
         updateDisplay(firstOperand || "0");
     } else if (secondOperand) {
-        secondOperand = secondOperand.slice(0, -1);
+        secondOperand = removeLastElement(secondOperand);
         updateDisplay(secondOperand);
     } else {
         operator = null;
